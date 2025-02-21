da86937a5f724202a2c85f6b7b7b6fe347574c0e
devark28 <e.shimwa1@alustudent.com>
2025-02-21 12:42:22 +0200

    UPD: correct autopep8 linking

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,14 +9,17 @@ import sys
 if __name__ == "__main__":
     employee_id = sys.argv[1]
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id)
 
     user_info = requests.request("GET", user_url).json()
     todo_info = requests.request("GET", todo_url).json()
 
     employee_name = user_info.get("name")
-    task_completed = list(filter(lambda x: (x["completed"] is True), todo_info))
+    task_completed = list(
+        filter(lambda x: (x["completed"] is True), todo_info))
     task_completed_count = len(task_completed)
     total_tasks = len(todo_info)
 
